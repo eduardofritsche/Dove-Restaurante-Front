3e2a604c8b029dbc02464ef83eea09d6dbca1c5a
Eduardo Henrique Fritsche <edu.fritsche@gmail.com>
2025-09-20 14:04:11 -0300

    Fix rota save cliente e hora_inicio dos pedidos

--- src/app/services/cliente.service.ts
+++ src/app/services/cliente.service.ts
@@ -15,8 +15,8 @@ export class ClienteService {
   findByEmail(email: string): Observable<Cliente> {
     return this.http.get<Cliente>(`${this.API}/findByEmail?email=${email}`);
   }
-  
-create(dto: Partial<Cliente>): Observable<Cliente> {
-    return this.http.post<Cliente>(this.API, dto);
+
+  create(dto: Partial<Cliente>): Observable<Cliente> {
+    return this.http.post<Cliente>(`${this.API}/save`, dto);
   }
 }
