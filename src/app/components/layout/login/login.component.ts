1f48b9210e7ded18d13d9f094506cd4966001fab
Eduardo Henrique Fritsche <edu.fritsche@gmail.com>
2025-11-21 21:13:49 -0300

    Add autenticação super básica

--- src/app/components/layout/login/login.component.ts
+++ src/app/components/layout/login/login.component.ts
@@ -3,15 +3,20 @@ import { Login } from '../../../auth/login';
 import { Router } from '@angular/router';
 import { LoginService } from '../../../auth/login.service';
 import Swal from 'sweetalert2';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-login: Login = new Login();
+  login: Login = new Login();
+  username: string = '';
+  password: string = '';
+  mostrarSenha: boolean = false;
 
   router = inject(Router);
 
@@ -25,16 +30,16 @@ login: Login = new Login();
 
   logar() {
 
-    this.loginService.logar(this.login).subscribe({
+    this.loginService.logar({username: this.username, password: this.password}).subscribe({
       next: token => {
 
         if (token)
           this.loginService.addToken(token); //MUITO IMPORTANTE
 
         this.gerarToast().fire({ icon: "success", title: "Seja bem-vindo!" });
-        this.router.navigate(['admin/dashboard']);
+        // this.router.navigate(['admin/dashboard']);
 
-        this.router.navigate(['/admin/carros']);
+        this.router.navigate(['/admin/pedidos']);
       },
       error: erro => {
         Swal.fire('Usuário ou senha incorretos!', '', 'error');
