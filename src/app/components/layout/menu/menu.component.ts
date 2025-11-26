be447c829f07301dc8c207118167fae2f99a1b3f
Eduardo Henrique Fritsche <edu.fritsche@gmail.com>
2025-11-25 21:18:26 -0300

    Add hasRole básico

--- src/app/components/layout/menu/menu.component.ts
+++ src/app/components/layout/menu/menu.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { RouterLink } from '@angular/router';
+import { LoginService } from '../../../auth/login.service';
 
 @Component({
   selector: 'app-menu',
@@ -10,6 +11,7 @@ import { RouterLink } from '@angular/router';
 })
 export class MenuComponent {
   router = inject(Router);
+  loginService = inject(LoginService)
 
   logout() {
     this.router.navigate(['/login']);
